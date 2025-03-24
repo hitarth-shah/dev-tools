c1724546aa241f08bae23944c0deea134ff37847
Hitarth Shah <hitarthshah24062001@gmail.com>
2025-03-24 18:12:59 +0530

    @vercel/speed-insights install

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -9,9 +9,7 @@ export const Heading = ({ heading, subHeading }: HeadingProps) => {
   return (
     <div className="flex flex-col gap-2 pb-8">
       <h5 className="text-4xl font-bold">{heading}</h5>
-      <p className="text-muted-foreground">
-        {subHeading}
-      </p>
+      <p className="text-muted-foreground">{subHeading}</p>
     </div>
   );
 };
